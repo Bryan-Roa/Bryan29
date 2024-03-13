edd9af8a404e4195e83c00375352e949da14cad4
Bryan-Roa <157432297+Bryan-Roa@users.noreply.github.com>
2024-03-13 07:25:57 +0100

    Add files via upload

---
+++ Programacion C/Programa 6.3/Programa 6.3.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+/* Universidad.
+El programa, al recibir informacion sobres el numero de alumnos que han ingresado a sus ocho
+diferentes carreras en los dos semestres lectivos de los ultimos cinco anos, obtiene carreras
+en los dos semestres lectivos de los ultimos cinco anos, obtiene informacion util para el departa
+mento de escolar.*/
+
+const int F = 8, C = 2, P = 5; /* Se declaran constantes para la fila, la columna y la profundudad.*/
+
+void Lectura(int [][C][P], int, int, int);
+void Funcion1(int [][C][P], int, int, int);
+void Funcion2(int [][C][P], int, int, int); /* Prototipo de funciones. cada prototipo de funcion corresponde a uno de los incisos*/
+void Funcion3(int [][C][P], int, int, int);
+
+void main(void)
+{
+    int UNI[F][C][P];
+    Lectura(UNI, F, C, P);
+    Funcion1(UNI, F, C, P);
+    Funcion2(UNI, F, C, P);
+    Funcion3(UNI, 6, C, P);
+
+    /* Se coloca el 6 como parametro ya que es la clave de la ingeniera en computacion.*/
+
+}
+
+void Lectura(int A[][C][P], int FI, int CO, int PR) /* La funcion lectura se utiliza para leer un arreglo tridimensional
+de tipo entero de Fi filas, co columnas y pr profudidad. observa que al ser tridimensional se necesitan tres ciclos para
+recorrer el arreglo.*/
+
+{
+    int K, I, J;
+    for (K=0; K<PR; K++)
+        for (I=0; I<FI; I++)
+        for (J=0; J<CO; J++)
+    {
+        printf("Ano: %d\tCarrera: %d\tSemestre: %d ", K+1, I+1, J+1);
+        scanf("%d", &A[I][J][K]);
+
+    }
+
+}
+
+void Funcion1(int A[][C][P],int FI, int CO, int PR) /* Esta funcion se utiliza para determinar el ano en el que ingrese el mayor numero
+ de alumnos a la universidad. observa que el primer ciclo externo corresponde a los anos.*/
+
+ {
+     int K, I, J, MAY = 0, AO = -1, SUM;
+     for (K=0; K<PR; K++)
+     {
+         SUM = 0;
+         for (I=0; I<FI; I++)
+            for (J=0; J<CO; J++)
+            SUM += A[I][J][K];
+         if (SUM > MAY)
+         {
+             MAY = SUM;
+             AO = K;
+         }
+     }
+     printf("\n\nAno con mayor ingreso de alumnos: %d Alumnos: %d", AO+1, MAY);
+ }
+
+ void Funcion2(int A[][C][P],int FI, int CO, int PR)
+
+ {
+     int I, J, MAY = 0, CAR = -1, SUM;
+     for (I=0; I<FI; I++)
+     {
+         SUM = 0;
+         for (J=0; J<CO; J++)
+         SUM += A[I][J][PR-1];
+         if (SUM > MAY)
+         {
+             MAY = SUM;
+             CAR = I;
+
+         }
+
+     }
+     printf("\n\nCarrera con mayor numero de alumnos: %d Alumnos: %d", CAR+1, MAY);
+
+ }
+
+ void Funcion3(int A[][C][P],int FI, int CO, int PR) /* Esta funcion se utiliza para determinar el ano en que la carrera
+ ingenieria en computacion recibio el mayor numero de alumnos. Observa que no */
+
+ {
+     int K, J, MAY = 0, AO = -1, SUM;
+     for (K=0; K<PR; K++)
+     {
+         SUM = 0;
+         for (J=0; J<CO; J++)
+            SUM += A[FI-1][J][K];
+         if (SUM > MAY)
+         {
+             MAY = SUM;
+             AO = K;
+         }
+     }
+     printf("\n\nAno con mayor ingreso de alumno: %d Alumnos: %d", AO+1, MAY);
+ }
+
+
+
+
+
+
+
